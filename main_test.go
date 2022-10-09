4acfd39a86ec92b13b84d13b1290ce89fe3ee280
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-10-09 16:02:19 -0400

    update-automated-save

--- main_test.go
+++ main_test.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/cwxstat/go-aws-s3-v2/common"
 	"github.com/rs/xid"
-	"s3-v2/common"
 )
 
 var client *s3.Client
